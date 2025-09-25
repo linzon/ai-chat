f68a9fb086a3bdb4c5fc7a3305f2efd52728923c
Btree <btree@Btrees-MacBook-Pro.local>
2025-09-25 15:32:36 +0800

    modify start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # install dependencies
-cd./ai-chat-ui && npminstall
-cd- && cd./backend-server && pipinstall-rrequirements.txt
+cd ./ai-chat-ui && npm install
+cd - && cd ./backend-server && pip install-rrequirements.txt
 
 
 # start ui
-cd- && cd./ai-chat-ui && npmrundev
+cd - && cd ./ai-chat-ui && npm run dev
 
 
 # start server
-cd- && cd./backend-server && python-muvicornapi.main:app--host0.0.0.0--port8000
+cd - && cd ./backend-server && python -m uvicorn api.main:app --host 0.0.0.0 --port 8000
